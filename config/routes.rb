1815b3bdc8e025e3d4b21534209f178ef04b33b0
keroloussamy <keroloussamy98@gmail.com>
2022-04-14 00:21:34 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/recipes/public', to: 'recipes#public'
   get '/foods/new(/:recipe_id)', to: 'foods#new'
   get '/general_shopping_list', to: 'shopping_list#index'
+  resources :recipe_foods
   resources :foods
   resources :recipes
   devise_for :users
